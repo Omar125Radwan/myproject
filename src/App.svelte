67563f987f46b6f3459e3ff90315e8955f113d3e
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2023-01-21 03:07:12 +0200

    props make reuseable component

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,10 @@
 			return person.id != id;
 		});
 	};
-	let num = 4;
 </script>
 
-<Modal />
+<!-- <Modal message="Hey, I am a prop value" isPromo="{true}"/> -->
+<Modal message="Hey, there again" />
 <main>
 	{#each pepole as person (person.id)}
 		<div>
@@ -36,12 +36,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 	@media (min-width: 640px) {
 		main {
