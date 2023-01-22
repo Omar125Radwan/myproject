dcdd2539833dbc8e5dc2d20b8638d3962b46875d
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2023-01-22 17:06:10 +0200

    Event Modifires

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,4 +1,7 @@
 <script>
+  // once - makes sure the event can only fire once (remove handler).
+  // preventDefault - prevent the default action (run e.preventDefault()).
+  // self - only fires the event if the clicked element is the target.
   export let message;
   export let showModal = false;
   export let isPromo = false;
@@ -6,7 +9,7 @@
 
 {#if showModal}
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="backdrop" class:promo={isPromo} on:click>
+<div class="backdrop" class:promo={isPromo} on:click|self>
   <div class="modal">
     <p>{message}</p>
   </div>
