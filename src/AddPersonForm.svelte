cc9250afdc5d8012591fb915e80c288e2ce935ae
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2023-01-24 23:39:18 +0200

    Forms 02 Select-box & Checkbox

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,18 +2,29 @@
   let name;
   let beltColor;
   let age;
+  let skills = [];
   const handleSubmit = () => {
-    console.log(name, beltColor, age);
-  }
+    console.log(name, beltColor, age, skills);
+  };
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
   <input type="text" placeholder="name" bind:value={name} />
-  <input type="text" placeholder="belt color" bind:value={beltColor} />
-  <input type="number" placeholder="age" bind:value={age}/>
+  <input type="number" placeholder="age" bind:value={age} />
+  <label for="">Skills:</label>
+  <input type="checkbox" bind:group={skills} value="fighting" /> Fighting<br>
+  <input type="checkbox" bind:group={skills} value="seeking" /> Seeking<br>
+  <input type="checkbox" bind:group={skills} value="running" /> Running<br>
+  <label for="">Belt Color</label>
+  <select bind:value={beltColor}>
+    <option value="black">black</option>
+    <option value="orange">orange</option>
+    <option value="brown">brown</option>
+    <option value="white">white</option>
+  </select>
+  <br />
   <button>Add Person</button>
 </form>
 
 <style>
-
 </style>
